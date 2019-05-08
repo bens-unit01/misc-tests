3460d4ceace134c84a13b4d00fa1b5b998402dfe
Messaoud <bensalem.messaoud@geothentic.com>
2019-05-08 16:18:16 -0400

    adding bluegiga app example

---
+++ bluegiga-bluetooth-tests/src/misc-tests.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : misc-tests.c
+ Author      : Raouf
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int m_test_01(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return 0;
+}
